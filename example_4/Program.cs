3720d69bb2bdef73ab9386f262c8acb91ad4c7ad
PavelSheglov <tpaul-1981@yandex.ru>
2018-05-07 14:05:41 +0700

    Лабораторная работа №2. Пример №4 - родительский класс

---
+++ example_4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new Found("father", 100);
+            Console.WriteLine("Объект базового класса");
+            f.Work();
+            Console.WriteLine();
+        }
+    }
+
+    public class Found
+    {
+        protected string name;
+        protected int credit;
+
+        public Found() { }
+
+        public Found(string name, int sum)
+        {
+            this.name = name; credit = sum;
+        }
+
+        public virtual void VirtMethod()
+        {   
+            Console.WriteLine("Отец: " + this.ToString());
+        }
+        
+        public override string ToString()
+        {
+            return (String.Format("поля: name = {0}, credit = {1}", name, credit));
+        }
+
+        public void NonVirtMethod()
+        {
+            Console.WriteLine("Мать: " + this.ToString());
+        }
+
+        public void Analysis()
+        {
+            Console.WriteLine("Простой анализ");
+        }
+
+        public void Work()
+        {
+            VirtMethod();
+            NonVirtMethod();
+            Analysis();
+        }
+    }
+
+}
